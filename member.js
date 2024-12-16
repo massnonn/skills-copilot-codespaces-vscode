ad422ebf4dde1f471035e92d2270fd92504a2dd8
massnonn <96299283+massnonn@users.noreply.github.com>
2024-12-16 15:29:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        dateOfBirth: new Date(1995, 2, 10)
+    };
+}
